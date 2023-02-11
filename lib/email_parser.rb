ebae8f38e12d52a2f70b82acd5091169ee87ffe1
Sereya Tiampati <sereyatiampatistudies@gmail.com>
2023-02-12 00:48:17 +0300

    learning RegEx

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,15 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+require 'pry'
+class EmailAddressParser
+    attr_reader :str
+    def initialize(str)
+        @str=str
+    end
+    def parse
+        arr= self.str.gsub(" ", ", ").split(/[,] /).delete(",")
+        arr
+    end
+end
+# binding.pry
